0d40235674ffb48472efdc235cbbe124833862ab
Highlander Vilela Paiva <highlanderpaiva.lemaf@gmail.com>
2018-07-31 04:41:12 -0300

    Escrevendo scripts de build, roullup module bundle e release

---
+++ build/release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+echo "Informe a versão da release: "
+read VERSION
+
+read -p "Releasing $VERSION - tem certeza? (s/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Ss]$ ]]
+then
+  echo "Releasing $VERSION ..."
+  # npm test
+  VERSION=$VERSION npm run build
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push
+  npm publish
+fi
